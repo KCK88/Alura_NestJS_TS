17a84ff7144146e692e72b109b801b07a3ce8310
Claudio <claudiomeirabs@gmail.comm>
2021-02-05 16:56:19 -0300

    Encerramento

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProdutosController } from './produtos.crontoller';
+import { Livro } from './livro.model';
+import { LivrosController } from './livros.crontoller';
+import { LivrosService } from './livros.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProdutosController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: process.env.USUARIOBD,
+      password: process.env.SENHADB,
+      database: 'livraria',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Livro])
+  ],
+  controllers: [AppController, LivrosController],
+  providers: [AppService, LivrosService],
 })
 export class AppModule {}
